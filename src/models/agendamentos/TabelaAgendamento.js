99d36270a378af1cbb10e4f63f4139d4d2c9d0be
Graciliana <eagraciliana@gmail.com>
2021-06-04 16:23:36 -0300

    funçoes sequelize criar métodos de acesso ao banco com sequelize

--- src/models/agendamentos/TabelaAgendamento.js
+++ src/models/agendamentos/TabelaAgendamento.js
@@ -1,7 +1,34 @@
 const Modelo = require('./modelTabelaAgendamento');
 
 module.exports = {
-    listar(){
-        return Modelo.findAll();
+    async listar(){
+        return await modeloAgendamento.findAll();
+    },
+    async adicionar(agendamento) {
+        return await modeloAgendamento.create(agendamento);
+    },
+    async buscarPorPK(id){
+        agendamento = await modeloAgendamento.findByPk(id);
+
+        return agendamento
+    },
+    async atualizar(id, dados){
+        return await modeloAgendamento.update(
+            dados,
+                {
+                    where: {
+                        id: id
+                    }
+                }
+        );
+    },
+    async removeEventListener(id) {
+        return await modeloAgendamento.destroy(
+            {
+                where: {
+                    id: id
+                }
+            }
+        );
     }
 };
