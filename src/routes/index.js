88d159a4e74752c3732ff5c91f695b35f0f2444a
Graciliana <eagraciliana@gmail.com>
2021-06-04 13:53:59 -0300

    utilização dos verbos HTTP

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 const TabelaAgendamento = require('./TabelaAgendamento');
+const Agendamento = require('./Agendamento');
+
+router.get('/agendamentos', async (req, resp) => {
+    const results = await TabelaAgendamento.listar();
+    resp.send(JSON.stringify(results));
+});
 
 router.use('/', async (req, resp) => {
     const result = await TabelaAgendamento.listar();
